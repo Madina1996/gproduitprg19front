e3d80c7088d71b2eab051a42e4d274e0cbaa4bb4
Madina1996 <modouserignediop@gmail.com>
2024-09-29 14:19:50 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { ProduitComponent } from './produit/produit.component';
+import { MouvementComponent } from './mouvement/mouvement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategorieComponent,
+    ProduitComponent,
+    MouvementComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mouvement/mouvement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mouvement',
+  templateUrl: './mouvement.component.html',
+  styleUrl: './mouvement.component.css'
+})
+export class MouvementComponent {
+
+}
